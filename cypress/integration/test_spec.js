b698d1c455c060bfea214408c7f8065aefb031e7
Even Haasted <even.haasted@nav.no>
2020-02-05 13:43:47 +0100

    lagt til en cypress test

---
+++ cypress/integration/test_spec.js
@@ -0,0 +1,8 @@
+/* eslint-disable no-undef */
+describe('Test test', () => {
+    it('Test 1', () => {
+        cy.visit('localhost:3000');
+        cy.get('[id=root]')
+            .should('contain', 'Edit');
+    });
+});
